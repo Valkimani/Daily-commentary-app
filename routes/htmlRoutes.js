6b541d3b7a49008180282770dc99e5848ee5a15c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-28 21:38:52 -0500

    Moved the files out of the develop folder and deleted the develop folder. Updated the paths in index.html and notes.html.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,8 @@
 const path = require("path");
 // Routing
 module.exports = function (app){
-    // HTML GET Requests
-    // Below code handles when users 'visit' a page.
-    // In each of the below cases the user is shown an HTML page of content
+    // HTML GET Requests when a user visits the page
+   
     
     app.get('/notes', function(request, response) {
         response.sendFile(path.join(__dirname, '../public/notes.html'));
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require("express");
 // inform node an express server has been created
 const app = express();
@@ -5,7 +6,17 @@ const app = express();
 // Sets the initial port
 const PORT = process.env.PORT || 3000;
 
+// express routes handling data parsing
+app.use(express.urlencoded({
+    extended:true
+}));
 
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+require ('/routes/apiRoutes')(app);
+require ('/routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
