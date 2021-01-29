c6dde8591843593b5ef61c7c8a0b70e76174ee1c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-28 21:10:38 -0500

    Created a html.Routes and added code from a class activity.Added a get request route in the apiRotes file.

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -2,16 +2,37 @@
 //linking our routes to a series of "data" sources.
 const fs =require ("fs")
 
+
+// npm package for unique id
+const { v4: uuidv4 } = require('uuid');
 // ROUTING
-module.exports = function(app) {
-  // API GET Requests
+module.exports = function (app) {
+    // API GET Request
+    app.get("/api/notes", (request, response) => {
+        
+        console.log("\n\nExecuting GET notes request");
+       
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        
+        console.log("\nGET request - Return notes data: " + JSON.stringify(data));
+        
+        // Send read data to response of 'GET' request
+        response.json(data);
+    });
+    
 
-  app.get("/api/notes", function(request, response) {
-    res.json(notes);
-  });
 
-  app.get("/api/", function(req, res) {
-    res.json(waitListData);
-  });
 
-  /
+
+
+
+
+
+
+
+
+
+
+  
+
+}
